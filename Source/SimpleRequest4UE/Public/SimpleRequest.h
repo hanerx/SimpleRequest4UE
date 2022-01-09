f242c103814ba460e521c0a7405fa44e0d35a4b9
hanerx <zhangyu199946@gmail.com>
2022-01-09 21:29:45 +0800

    Init

---
+++ Source/SimpleRequest4UE/Public/SimpleRequest.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "SimpleRequest.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLEREQUEST4UE_API USimpleRequest : public UObject
+{
+	GENERATED_BODY()
+};
